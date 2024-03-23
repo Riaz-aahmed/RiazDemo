a8c07d409fb218701cf0c480311047c52e39af15
Riaz-aahmed <164642717+Riaz-aahmed@users.noreply.github.com>
2024-03-23 07:35:53 -0700

    Add project files.

---
+++ RiazDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RiazDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ RiazDemo/Models/MydatabaseContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RiazDemo.Models;
+
+public partial class MydatabaseContext : DbContext
+{
+    public MydatabaseContext()
+    {
+    }
+
+    public MydatabaseContext(DbContextOptions<MydatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-OMQ89VA\\SQLEXPRESS;Initial Catalog=mydatabase;Integrated Security=True;Trust Server Certificate=True\n");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC07ABAB6292");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Age).HasColumnName("AGE");
+            entity.Property(e => e.Contact)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("CONTACT");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("NAME");
+            entity.Property(e => e.Semester)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("SEMESTER");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
